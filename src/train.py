31c52e6fa292dab43411b6b9b685e30f396d035f
Taahia23 <taahiatahsin@gmail.com>
2026-03-28 02:20:18 +0600

    train data loaded by calling imageDataGenerator function

--- src/train.py
+++ src/train.py
@@ -4,3 +4,11 @@ from tensorflow.keras import layers, models
 
 train_dir = "../dataset/fer2013/train"
 test_dir = "../dataset/fer2013/test"
+
+train_data = ImageDataGenerator(rescale=1./255).flow_from_directory(
+    train_dir,
+    target_size=(48,48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode="categorical"
+)
