4e6aa2f7c645c005a00e15ddcfba197a829df89a
Taahia23 <taahiatahsin@gmail.com>
2026-03-28 02:19:15 +0600

    added dataset directory for train and test set

--- src/train.py
+++ src/train.py
@@ -1,3 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers, models
+
+train_dir = "../dataset/fer2013/train"
+test_dir = "../dataset/fer2013/test"
