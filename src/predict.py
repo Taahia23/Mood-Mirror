908b63324046502a1ecbb10249489a7fc63f69f6
Taahia23 <taahiatahsin@gmail.com>
2026-03-30 23:34:37 +0600

    set suggestions for four labels

--- src/predict.py
+++ src/predict.py
@@ -5,3 +5,10 @@ from tensorflow.keras.models import load_model
 model = load_model("../models/emotion_model.h5")
 
 labels = ["Angry", "Happy", "Neutral", "Sad"]
+
+suggestions = {
+    "Angry": "Try deep breathing",
+    "Happy": "Keep smiling",
+    "Neutral": "Stay positive",
+    "Sad": "Take a break & talk to someone"
+}
