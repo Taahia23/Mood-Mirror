732c072d06d82e11c118488a436e4f11c07f6ec2
Taahia23 <taahiatahsin@gmail.com>
2026-04-02 01:11:27 +0600

    function define done for image mode

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ from src.predict import predict_emotion
 st.title("Emotion Detection and Mental Health Support")
 
 option = st.radio("Choose option:", ["Upload Image", "Webcam"])
+
+# 📷 IMAGE MODE
+if option == "Upload Image":
+    uploaded_file = st.file_uploader("Upload Image", type=["jpg","png"])
+
+    if uploaded_file:
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        img = cv2.imdecode(file_bytes, 1)
+
+        emotion, suggestion = predict_emotion(img)
+
+        st.image(img, channels="BGR")
+        st.write(f"### Emotion: {emotion}")
+        st.write(f"Suggestion: {suggestion}")
